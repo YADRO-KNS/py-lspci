d1f77c0faac50952e40ae7381cad17f57d276385
Sergey Parshin <s.parshin@yadro.com>
2019-11-27 14:19:13 +0300

    Version increase

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as fp:
 
 setuptools.setup(
     name="py-lspci",
-    version="0.0.1",
+    version="0.0.2",
     author="Sergey Parshin",
     author_email="s.parshin@yadro.com",
     description="Parser for lspci output on remote or local machines",
