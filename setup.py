51666681f918c36879021d33b30b1ae326914b05
Sergey Parshin <s.parshin@yadro.com>
2019-11-27 16:12:48 +0300

    Actions updated to run test on 3.6, 3.7 and 3.8 python versions

--- setup.py
+++ setup.py
@@ -19,7 +19,9 @@ setuptools.setup(
     install_requires=install_requires,
     classifiers=[
         "Development Status :: 4 - Beta",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: Unix",
     ]
